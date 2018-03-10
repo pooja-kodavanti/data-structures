3d9930a42b654a48ff0614015cb9e581f5da42f8
pooja-kodavanti <pkodavan@gmail.com>
2018-03-09 16:07:50 -0800

    Complete Basic Requirements

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,75 @@
 var BinarySearchTree = function(value) {
+  this.left = null;
+  this.right = null;
+  this.value = value;
+  
 };
 
+
+
+// O(log n)
+BinarySearchTree.prototype.insert = function(value) {
+  var node = this;
+  if (value < node.value) {
+    if (!node.left) {
+    node.left = new BinarySearchTree(value);
+    } else {
+    node.left.insert(value);
+    }
+  }
+  if (value > node.value) {
+    if (!node.right) {
+      node.right = new BinarySearchTree(value);
+    } else {
+      node.right.insert(value);
+    }
+  }
+};
+
+// O(log n)
+BinarySearchTree.prototype.contains = function(target) {
+  var hasNode = false;
+  console.log(this.right);
+  //if node value is equal to target, return true
+  //if node value < target, return .contains(node.left) (vv for right)
+  var checkTree = function(node) {
+    // console.log(node)
+    if (node.value === target) {
+      hasNode = true;
+    }
+
+    if (target < node.value) {
+      if (node.left) {
+        checkTree(node.left);
+        console.log(checkTree(node.left))
+      }
+    }
+    if (target > node.value) {
+      if (node.right) {
+        checkTree(node.right);
+      }
+    }
+  }
+  checkTree(this);
+  return hasNode;
+  
+};
+
+
+// O(n)
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  var logItems = function(node) {
+    // console.log(node)
+    cb(node.value);
+    if (node.left) {
+      logItems(node.left);
+    }
+    if (node.right) {
+      logItems(node.right)
+    }
+  }
+  logItems(this);
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,25 +6,47 @@ var HashTable = function() {
 
 };
 var hash = new HashTable;
-// console.log(hash)
+
+// Big-O O(n)
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return index;
-  // console.log(k);
-  // console.log(v);
-  // console.log(index);
+  var tuple = [k, v];
+
+  if (!this._storage[index]) {
+    this._storage[index] = [tuple];
+  }
+  
+  for (var i = 0; i < this._storage[index].length; i++) {
+    if (this._storage[index][i][0] === k) {
+      this._storage[index][i][1] = v;
+    } else {
+      this._storage[index].push(tuple);
+    }
+  }
 };
 
+// Big-O O(n)
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  console.log(k);
+
+  if (!this._storage[index]) {
+    return undefined;
+  }
   
-  console.log(index);
-  return index;
+  var bucket = this._storage[index];
+
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      return bucket[i][1]; 
+    }
+  }
+  return undefined;
 };
 
+// Big-O O(1)
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[index] = undefined;
 };
 
 
